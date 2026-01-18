c87a435345e8820d99c88aa302d39614a474e4e2
Elieser <66266691+elieserme@users.noreply.github.com>
2026-01-18 19:51:51 -0300

    abas

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,20 @@ import streamlit as st
 df = yf.download("^BVSP", period="2y", interval="1d")
 df.dropna(inplace=True)
 
-st.dataframe(df)
+st.title("📈 Análise de índices da Bolsa")
+
+aba1, aba2 = st.tabs(["📈 Gráfico", "📊 Dados brutos"])
+
+with aba1:
+    st.write("Gráfico de preços de fechamento do índice Bovespa nos últimos 2 anos")
+    st.line_chart(df['Close'])
+    
+with aba2:
+    st.write("Dados brutos")
+    rows, columns = df.shape
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Número de linhas", rows)
+    with col2:
+        st.metric("Número de colunas", columns)    
+    st.dataframe(df)
